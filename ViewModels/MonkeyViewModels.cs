3da21350db8be59c006187f1b43844251199d23e
User <User@LAB2-1>
2024-01-24 09:43:08 +0200

    guhhyhyuhyh

---
+++ ViewModels/MonkeyViewModels.cs
@@ -0,0 +1,61 @@
+﻿using MonkeysMVVM.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonkeysMVVM.ViewModels
+{
+    internal class MonkeyViewModels: ViewModelBase
+    {
+        private string name;
+        public string Name
+        {
+            get { return this.name; }
+            set
+            {
+                this.name = value;
+                OnPropertyChanged();
+            }
+        }
+        private string location;
+        public string Location
+        {
+            get { return this.location; }
+            set
+            {
+                this.location = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string imageUrl;
+        public string ImageUrl
+        {
+            get { return this.imageUrl; }
+            set
+            {
+                this.imageUrl = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public Command GetMonkeyCommand { get; set; }
+        public string locationentry;
+        public string LocationEntry
+        {
+            get { return this.LocationEntry; }  
+        }
+        private void GetMonkey()
+        {
+           List<Monkey> monkeys = new List<Monkey>();                                                                                     
+        }
+        private int numberOfMonkeysInLocation;
+        public int NumberOfMonkeysInLocation 
+        {
+            get { return this.numberOfMonkeysInLocation; }
+            set { this.numberOfMonkeysInLocation = value; OnPropertyChanged(); }
+        }
+    }
+}
